a720233aa65289e37da81d14e70d020b40c6fc15
Neketek <neketek@gmail.com>
2019-10-20 18:24:29 +0300

    Completed first version of filestore

--- src/filestore/__init__.py
+++ src/filestore/__init__.py
@@ -1,6 +1,10 @@
-import tempfile
 import os
+import tempfile
 import posixpath
+import hashlib
+import json
+import time
+import datetime
 
 
 class Filestore:
@@ -10,20 +14,31 @@ class Filestore:
     internally converted in case of win system.
     """
 
-    def __init__(self, name, root=None):
-        self.name = name
+    METADATA_MD5_KEY = 'md5'
+    # modification timestamp
+    METADATA_MTIMESTAMP_KEY = 'mtimestamp'
+    METADATA_CONTENTSIZE_KEY = 'content_size'
+
+    def __init__(self, name, root=None, metadata_root=None):
+        self._name = name
         if root:
-            self.dir = os.mkdir(os.path.join(root, name))
+            self._dir = os.path.join(root, name)
+            os.makedirs(self._dir, exist_ok=True)
         else:
-            self.temp_dir = tempfile.TemporaryDirectory()
-            self.dir = self.temp_dir.name
+            self._temp_dir = tempfile.TemporaryDirectory()
+            self._dir = self._temp_dir.name
+        if metadata_root:
+            self._metadata_dir = os.path.join(metadata_root, name)
+            os.makedirs(self._metadata_dir, exist_ok=True)
+        else:
+            self._temp_metadata_dir = tempfile.TemporaryDirectory()
+            self._metadata_dir = self._temp_metadata_dir.name
 
     def _convert_key_to_path(self, key):
         # removes leading slash and converts to current system sep
         return os.path.sep.join(
             [
-                part for part in key.split(posixpath.sep)
-                if part
+                part for part in key.split(posixpath.sep) if part
             ]
         )
 
@@ -33,45 +48,129 @@ class Filestore:
 
     def _convert_key_to_fullpath(self, key):
         return os.path.join(
-            self.dir,
+            self._dir,
             self._convert_key_to_path(key)
         )
 
+    def _convert_key_to_metadata_fullpath(self, key):
+        return os.path.join(
+            self._metadata_dir,
+            self._convert_key_to_path(key)
+        )
+
+    def _generate_file_metadata(self, key, byte_value):
+        md5_hash = str(hashlib.md5(byte_value).hexdigest())
+        return json.dumps(
+            {
+                self.METADATA_MD5_KEY: md5_hash,
+                self.METADATA_MTIMESTAMP_KEY: time.time(),
+                self.METADATA_CONTENTSIZE_KEY: len(byte_value)
+            }
+        ).encode()
+
+    def _clear_directory(self, path):
+        for root, dirnames, filenames in os.walk(path, False):
+            for filename in filenames:
+                os.remove(
+                    os.path.join(root, filename)
+                )
+            for dirname in dirnames:
+                os.rmdir(
+                    os.path.join(root, dirname)
+                )
+
     def post(self, key, byte_value):
+        # Note: I know that this is not very optimized,
+        # but I don't care because performance reduction is minimal
         filename = self._convert_key_to_fullpath(key)
+        metadata_filename = self._convert_key_to_metadata_fullpath(key)
+        metadata_bytes = self._generate_file_metadata(key, byte_value)
         dirname = os.path.dirname(filename)
+        metadata_dirname = os.path.dirname(metadata_filename)
         if dirname:
             os.makedirs(dirname, exist_ok=True)
+        if metadata_filename:
+            os.makedirs(metadata_dirname, exist_ok=True)
         with open(filename, 'wb') as f:
             f.write(byte_value)
+        with open(metadata_filename, 'wb') as f:
+            f.write(metadata_bytes)
         return True
 
     def delete(self, key):
         try:
             filename = self._convert_key_to_fullpath(key)
+            metadata_filename = self._convert_key_to_metadata_fullpath(key)
             os.remove(filename)
+            os.remove(metadata_filename)
         except FileNotFoundError:
             return False
         return True
 
-    def get(self, key):
+    def get(
+        self,
+        key,
+        if_match_md5=None,
+        if_unchanged_since=None
+    ):
+        filename = self._convert_key_to_fullpath(key)
+        metadata_filename = self._convert_key_to_metadata_fullpath(key)
         try:
-            filename = self._convert_key_to_fullpath(key)
-            return open(filename, 'rb')
+            metadata = json.load(open(metadata_filename, 'rb'))
+            body = open(filename, 'rb')
+            if (
+                if_match_md5 is not None
+                and metadata[self.METADATA_MD5_KEY] != if_match_md5
+            ):
+                return None
+            if (
+                if_unchanged_since is not None
+                and datetime.datetime.fromtimestamp(
+                    metadata[self.METADATA_MTIMESTAMP_KEY]
+                ) > if_unchanged_since
+            ):
+                return None
+            return body
         except FileNotFoundError:
             return None
+
+    def head(self, key):
+        try:
+            filename = self._convert_key_to_metadata_fullpath(key)
+            with open(filename, 'rb') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return None
+
+    def exists(self, key):
+        try:
+            filename = self._convert_key_to_fullpath(key)
+            os.stat(filename)
+            return os.path.isfile(filename)
+        except FileNotFoundError:
+            return False
 
     def list(self, key_prefix=''):
         path_prefix = self._convert_key_to_path(key_prefix)
 
         def iterator():
-            top = os.path.join(self.dir, path_prefix)
+            top = os.path.join(self._dir, path_prefix)
             for root, dirnames, filenames in os.walk(top):
                 for filename in filenames:
                     yield self._convert_path_to_key(
                         os.path.relpath(
                             os.path.join(root, filename),
-                            self.dir
+                            self._dir
                         )
                     )
         return iterator()
+
+    def count(self, key_prefix=''):
+        count_value = 0
+        for file in self.list(key_prefix):
+            count_value += 1
+        return count_value
+
+    def clear(self):
+        self._clear_directory(self._dir)
+        self._clear_directory(self._metadata_dir)
--- tests/unit/filestore/test.py
+++ tests/unit/filestore/test.py
@@ -1,4 +1,9 @@
-from src.loggers import root
+import os
+import time
+import tempfile
+import datetime
+import hashlib
+import freezegun
 from src.filestore import Filestore
 
 
@@ -39,10 +44,201 @@ def test_crud_temp_dir():
         assert filestore.get(key) is None
 
 
-def test_list():
+def test_list_count():
+    filestore = Filestore('test')
+    objs = {
+        '': generate_objects(
+            10,
+            'item-{i}',
+            'item-{i}-value'
+        ),
+        'prefix-1': generate_objects(
+            10,
+            'prefix-1/item-{i}',
+            'prefix-1-item-{i}-value'
+        ),
+        'prefix-2/prefix': generate_objects(
+            5,
+            'prefix-2/prefix/item-{i}',
+            'prefix-2-item-{i}-value'
+        )
+    }
+    all_objs = {
+        **objs[''],
+        **objs['prefix-1'],
+        **objs['prefix-2/prefix']
+    }
+    for prefix in objs.values():
+        for key, value in prefix.items():
+            filestore.post(
+                key,
+                value
+            )
+    # testing count method
+    assert filestore.count() == 25
+    assert filestore.count('prefix-1') == 10
+    assert filestore.count('prefix-2/prefix') == 5
+
+    # testing list method
+    count = 0
+    for key in filestore.list():
+        assert key in all_objs
+        count += 1
+    assert count == 25
+
+    count = 0
+    for key in filestore.list('prefix-1'):
+        assert key in objs['prefix-1']
+        count += 1
+    assert count == 10
+
+    count = 0
+    for key in filestore.list('prefix-2/prefix'):
+        assert key in objs['prefix-2/prefix']
+        count += 1
+    assert count == 5
+
+
+def test_clear():
     filestore = Filestore('test-storage')
-    objs = generate_objects(10, 'directory-{i}/item-{i}', 'item-{i}-value')
+    objs = {
+        **generate_objects(
+            30,
+            'directory-{i}/item-{i}',
+            'item-{i}-value'
+        ),
+        **generate_objects(
+            10,
+            'directory-{i}/directory-{i}/item-{i}',
+            'item-{i}-value'
+        )
+    }
     for key, value in objs.items():
         filestore.post(key, value)
-    for key in filestore.list(''):
-        root.info(key)
+    assert filestore.count() == len(objs)
+    filestore.clear()
+    assert filestore.count() == 0
+
+
+def test_conditional_get():
+    filestore = Filestore('test')
+    key = 'test/conditional/get/item'
+    value = b'test-item-value'
+    filestore.post(key, value)
+    # checking regular get and head to obtain metadata
+    assert filestore.get(key).read() == value
+    metadata = filestore.head(key)
+    assert metadata
+    # extracting required data from metadata
+    mtimestamp = metadata[filestore.METADATA_MTIMESTAMP_KEY]
+    md5hash = metadata[filestore.METADATA_MD5_KEY]
+    md5hash_modified = md5hash[:-1]
+    # datetime conditions
+    mdatetime = datetime.datetime.fromtimestamp(mtimestamp)
+    before_mdatetime = mdatetime - datetime.timedelta(days=1)
+    after_mdatetime = mdatetime + datetime.timedelta(days=1)
+    # must return value exact time match
+    assert filestore.get(
+        key,
+        if_unchanged_since=mdatetime
+    ) is not None
+    # must return value condition is after mtimestamp
+    assert filestore.get(
+        key,
+        if_unchanged_since=after_mdatetime
+    ) is not None
+    # must not return value, condition is before mtimestamp
+    assert filestore.get(
+        key,
+        if_unchanged_since=before_mdatetime
+    ) is None
+    # must return value hash is correct
+    assert filestore.get(
+        key,
+        if_match_md5=md5hash
+    ) is not None
+    # must not return value hash is modified
+    assert filestore.get(
+        key,
+        if_match_md5=md5hash_modified
+    ) is None
+
+
+@freezegun.freeze_time()
+def test_head_metadata():
+    filestore = Filestore('test')
+    key = 'item'
+    value = b'item-value'
+    filestore.post(key, value)
+    # head method return metadata json of requested key
+    metadata = filestore.head(key)
+    assert (
+        metadata[filestore.METADATA_MD5_KEY]
+        == str(hashlib.md5(value).hexdigest())
+    )
+    assert (
+        metadata[filestore.METADATA_CONTENTSIZE_KEY]
+        == len(value)
+    )
+    assert metadata[filestore.METADATA_MTIMESTAMP_KEY] == time.time()
+    assert filestore.delete(key)
+    assert filestore.head(key) is None
+
+
+def test_exists():
+    filestore = Filestore('test')
+    directory = 'directory'
+    name = 'item'
+    key = f'{directory}/{name}'
+    value = b'item-value'
+
+    # no files in filestore, nothing exist
+    assert not filestore.exists(key)
+    assert not filestore.exists(directory)
+
+    filestore.post(key, value)
+    # file posted therefore exists
+    assert filestore.exists(key)
+    # directory is not recognized as an independed object only as part of a key
+    # therefore doesn't exist
+    assert not filestore.exists(directory)
+    assert filestore.delete(key)
+    # file removed therefore nothing exist
+    assert not filestore.exists(key)
+    assert not filestore.exists(directory)
+
+
+def test_specific_directories():
+    metadata_dir = tempfile.TemporaryDirectory()
+    data_dir = tempfile.TemporaryDirectory()
+    filestore_name = 'test'
+    filestore = Filestore(filestore_name, data_dir.name, metadata_dir.name)
+    key = 'item'
+    value = b'item-value'
+    expected_data_directory = os.path.join(
+        data_dir.name,
+        filestore_name
+    )
+    expected_metadata_directory = os.path.join(
+        metadata_dir.name,
+        filestore_name
+    )
+    # directories created before file post
+    assert os.path.exists(expected_data_directory)
+    assert os.path.isdir(expected_data_directory)
+    assert os.path.exists(expected_metadata_directory)
+    assert os.path.isdir(expected_metadata_directory)
+    expected_data_file_path = os.path.join(
+        expected_data_directory,
+        key
+    )
+    expected_metadata_file_path = os.path.join(
+        expected_metadata_directory,
+        key
+    )
+    filestore.post(key, value)
+    # data and metadata files created
+    assert os.path.exists(expected_data_file_path)
+    assert os.path.isfile(expected_data_file_path)
+    assert os.path.exists(expected_metadata_file_path)
+    assert os.path.isfile(expected_metadata_file_path)
